f28c8c380ccde0a934edca319746bab900ea85a4
Allan Mwatha <161926520+Mwatha40@users.noreply.github.com>
2024-05-22 16:10:53 +0300

    Update AccountContainer.js

--- src/components/AccountContainer.js
+++ src/components/AccountContainer.js
@@ -1,10 +1,12 @@
-import React from "react";
+import React, { useState } from "react";
 import TransactionsList from "./TransactionsList";
 import Search from "./Search";
 import AddTransactionForm from "./AddTransactionForm";
 import App from "./App";
 
 function AccountContainer() {
+  const [transactions, setTransactions] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("");
   return (
     <div>
       {/* <Search onSearching={handleSearching}/> *//}
